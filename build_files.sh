7a3f0ab7fc5c541352779d742c9ebacfcbb359f6
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2026-09-21 18:46:17 -0300

    adicionando variaveis e subindo primeiro deployAAb

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+set -e
+
+export PIP_DISABLE_PIP_VERSION_CHECK=1
+
+python3 -m pip install --break-system-packages -r requirements.txt
+
+python3 manage.py migrate --noinput
+
+
+python3 manage.py collectstatic --noinput --upload-unhashed-files
